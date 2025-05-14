5c33be5f443711cf272042a694d26ac09fd51798
Ramon <carrillozamudiojesusramon@gmail.com>
2025-05-13 18:19:52 -0700

    Se intento solucionar work para que agregue Solo se agrego que el "Ok" cerrara y terminara la ejecucion

--- src/main/java/mx/itson/cajero/ui/SaldoFrame.java
+++ src/main/java/mx/itson/cajero/ui/SaldoFrame.java
@@ -107,7 +107,7 @@ public class SaldoFrame extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
